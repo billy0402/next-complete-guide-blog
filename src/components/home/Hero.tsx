9a39b0c3a50be89f7d44349f29dd340c378a434c
Billy Huang <10646003@ntub.edu.tw>
2022-02-09 00:17:38 +0800

    feat(Hero): Adding The "Hero" Component

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -1,5 +1,23 @@
+import Image from 'next/image';
+
 const Hero = () => {
-  return <>Hero</>;
+  return (
+    <section className='hero'>
+      <div className='hero__image'>
+        <Image
+          src='/images/home/billy.jpg'
+          alt='A image showing Billy'
+          width={300}
+          height={300}
+        />
+      </div>
+      <h1>Hi, I&apos;m Billy</h1>
+      <p>
+        I blog about web development - especially frontend frameworks like
+        Angular or React.
+      </p>
+    </section>
+  );
 };
 
 export default Hero;
