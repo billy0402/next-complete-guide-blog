9afb774e4070f4df98b865ba719cc159a1a58fc6
Billy Huang <10646003@ntub.edu.tw>
2022-02-09 15:58:50 +0800

    feat(PostDetail): Rendering Images With The "Next Image" Component (From Markdown)

--- src/components/post/post-detail/PostDetail.tsx
+++ src/components/post/post-detail/PostDetail.tsx
@@ -1,4 +1,6 @@
-import ReactMarkdown from 'react-markdown';
+import Image from 'next/image';
+
+import ReactMarkdown, { Components } from 'react-markdown';
 
 import { Post } from '@models/post';
 import PostHeader from './PostHeader';
@@ -8,10 +10,34 @@ type Props = {
 };
 
 const PostDetail = ({ post }: Props) => {
+  const customComponents: Components = {
+    p: ({ node, children }) => {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      const nodeFirstChild: any = node.children[0];
+      if (nodeFirstChild.tagName === 'img') {
+        const image = nodeFirstChild;
+        return (
+          <div className='post-detail__image'>
+            <Image
+              src={`/images/post/${post.slug}/${image.properties.src}`}
+              alt={image.alt}
+              width={600}
+              height={300}
+            />
+          </div>
+        );
+      }
+
+      return <p>{children}</p>;
+    },
+  };
+
   return (
     <article className='post-detail'>
       <PostHeader {...post} />
-      <ReactMarkdown>{post.content}</ReactMarkdown>
+      <ReactMarkdown components={customComponents}>
+        {post.content}
+      </ReactMarkdown>
     </article>
   );
 };
